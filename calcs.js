c95cc8c263455cb6b1252ee3bd440e8a380c3468
NinjaMKII <kevinjkramer168@gmail.com>
2020-07-29 14:26:01 -0400

    Tutorial with extremely basic javascript

---
+++ calcs.js
@@ -0,0 +1,21 @@
+console.log("Code Differently");
+console.log(3 + 4);
+console.log(5- 3)
+console.log(3 * 3)
+console.log(12 / 2)
+// ----------------------------------------------------------------------------------------------------
+var x = 10, y = 2;
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+//-----------------------------------------------------------------------------------------------------
+var x = 72, y = 8;
+console.log(x + y)
+console.log(x - y)
+console.log(x * y)
+console.log(x / y)
+for(i = 5; i <= 15; i+=2){
+    console.log(i);
+}
+
