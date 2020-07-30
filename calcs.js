e058e8535f0333c3184b86f8f7910454cdb6ec8d
NinjaMKII <kevinjkramer168@gmail.com>
2020-07-30 16:28:06 -0400

    loops with if and else statements

--- calcs.js
+++ calcs.js
@@ -1,24 +1,41 @@
-console.log("Code Differently");
-console.log(3 + 4);
-console.log(5- 3)
-console.log(3 * 3)
-console.log(12 / 2)
-// ----------------------------------------------------------------------------------------------
-var x = 10, y = 2;
-console.log(x + y);
-console.log(x - y);
-console.log(x * y);
-console.log(x / y);
-for(i = 1; i <= 5; i++){
-    console.log(i);
-}
-//-----------------------------------------------------------------------------------------------
-var x = 72, y = 8;
-console.log(x + y)
-console.log(x - y)
-console.log(x * y)
-console.log(x / y)
-for(i = 5; i <= 15; i+=2){
-    console.log(i);
-}
-
+// console.log("Code Differently");
+// console.log(3 + 4);
+// console.log(5- 3)
+// console.log(3 * 3)
+// console.log(12 / 2)
+// // ----------------------------------------------------------------------------------------------
+// var x = 10, y = 2;
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// for(i = 1; i <= 5; i++){
+//     console.log(i);
+// }
+// //-----------------------------------------------------------------------------------------------
+// var x = 72, y = 8;
+// console.log(x + y)
+// console.log(x - y)
+// console.log(x * y)
+// console.log(x / y)
+// for(i = 5; i <= 15; i+=2){
+//     console.log(i);
+// }
+// //-----------------------------------------------------------------------------------------------
+// var firstName = "Kevin";
+// var lastName = "Kramer";
+// var favoriteCareer = "software developer"
+// console.log('Hi, my name is ' + firstName + ' ' + lastName + '.' + ' I aspire to be a ' + favoriteCareer + ', whether its video games or other software, being able to work with technology is my dream.');   
+var firstName = "Kevin "
+var lastName = "Kramer"
+var fullName = firstName.concat(lastName)
+console.log(fullName)
+for(i = 1; i<=100; i++){
+    if(i%3==0){
+        console.log('Buzz')
+        }
+    else{
+        console.log(i)
+    }
+    
+} 
